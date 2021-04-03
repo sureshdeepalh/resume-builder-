db40e949db18008ca3079cf87fc892878900e7b3
Suresh <suresh@efuturesworld.com>
2021-04-03 20:17:43 +0530

    release bracnh init

--- pages/index.js
+++ pages/index.js
@@ -3,23 +3,6 @@ import Head from "next/head";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  const [data, setData] = useState(null);
-  const getDate = useCustomFunc();
-  let [count, setCount] = useState(0);
-
-  useEffect(() => {
-    setCount(count++);
-  });
-
-  useEffect(() => {
-    fetch("/api/hello")
-      .then((req) => req.json())
-      .then((data) => setData(data));
-  }, []);
-
-  const handleOnChange = () => {
-    setData(null);
-  };
   return (
     <div className={styles.container}>
       <Head>
@@ -27,21 +10,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <h1>{data?.name}</h1>
-        <h2>{getDate}</h2>
-        {count}
-        <button onClick={handleOnChange}>Click me</button>
+        <h1>Hello this is release branch</h1>
       </main>
     </div>
   );
 }
-
-const useCustomFunc = () => {
-  const [date, setDate] = useState(null);
-  let [count, setCount] = useState(0);
-
-  useEffect(() => {
-    setCount(count++);
-  });
-  return count;
-};
