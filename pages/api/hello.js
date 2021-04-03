dadd117d17bbeff58f6328c3563bfe15853ee344
Suresh <suresh@efuturesworld.com>
2021-04-03 20:41:48 +0530

    add api console

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default (req, res) => {
-  res.status(200).json({ name: 'John Doe' })
-}
+  console.log("This is test console");
+  res.status(200).json({ name: "John Doe" });
+};
