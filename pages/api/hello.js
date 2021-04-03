b95e1f2e4756ea449b0b091ea78bb073ee281b24
Suresh <suresh@efuturesworld.com>
2021-04-03 20:47:16 +0530

    test request body

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default (req, res) => {
-  console.log("This is test console");
+  console.log(req.body);
+
   res.status(200).json({ name: "John Doe" });
 };
